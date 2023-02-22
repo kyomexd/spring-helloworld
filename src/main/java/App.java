91493aa084743a0c037e8c937e077ab4df9bb981
kyomexd <matveev.works@gmail.com>
2023-02-23 01:04:50 +0300

    2.1.2 solution

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,12 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld anotherBean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        Cat anotherCatBean = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean == anotherBean);
+        System.out.println(catBean == anotherCatBean);
     }
 }
